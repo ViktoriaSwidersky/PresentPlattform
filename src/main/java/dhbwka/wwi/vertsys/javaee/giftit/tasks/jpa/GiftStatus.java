4611b8e7d44cf3026bd829be99d7af7837c3c02e
Sven <sven@zelinka.de>
2019-03-25 14:07:12 +0100

    GiftitStatus anlegen

---
+++ src/main/java/dhbwka/wwi/vertsys/javaee/giftit/tasks/jpa/GiftStatus.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright © 2018 Dennis Schulmeister-Zimolong
+ *
+ * E-Mail: dhbw@windows3.de
+ * Webseite: https://www.wpvs.de/
+ *
+ * Dieser Quellcode ist lizenziert unter einer
+ * Creative Commons Namensnennung 4.0 International Lizenz.
+ */
+package dhbwka.wwi.vertsys.javaee.giftit.tasks.jpa;
+
+/**
+ * Statuswerte einer Aufgabe.
+ */
+public enum GiftStatus {
+    IDEA, PLATFORM_FOUND, IN_ORDER, DELIVERD, CANCELED;
+
+    /**
+     * Bezeichnung ermitteln
+     *
+     * @return Bezeichnung
+     */
+    public String getLabel() {
+        switch (this) {
+            case IDEA:
+                return "Idee";
+            case PLATFORM_FOUND:
+                return "Bestellplattform gefunden";
+            case IN_ORDER:
+                return "Bestellt";
+            case CANCELED:
+                return "Verworfen";
+            case DELIVERD:
+                return "Ausgeliefert";
+            default:
+                return this.toString();
+        }
+    }
+
+}
