3869a2b6706df19f5b617ec68ea09e0c1b917940
Viktoria Swidersky <viktoria.swidersky@gmx.de>
2019-03-28 13:25:52 +0100

    WebService

---
+++ src/main/java/dhbwka/wwi/vertsys/javaee/giftit/soap/GiftIt.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2019 Dennis Schulmeister-Zimolong
+ * 
+ * E-Mail: dhbw@windows3.de
+ * Webseite: https://www.wpvs.de/
+ * 
+ * Dieser Quellcode ist lizenziert unter einer
+ * Creative Commons Namensnennung 4.0 International Lizenz.
+ */
+package dhbwka.wwi.vertsys.javaee.giftit.soap;
+
+import dhbwka.wwi.vertsys.javaee.giftit.common.ejb.UserBean_giftit;
+import dhbwka.wwi.vertsys.javaee.giftit.tasks.ejb.GiftBean_giftit;
+import dhbwka.wwi.vertsys.javaee.giftit.tasks.jpa.Gift_giftit;
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Viktoria
+ */
+@Stateless
+public class GiftIt{
+    
+    @EJB
+    private GiftBean_giftit giftBean;
+    
+    @WebMethod
+    @WebResult(name = "gifts")
+    public List<Gift_giftit> findallgifts(){
+        return giftBean.findAll();
+    }
+          
+}
